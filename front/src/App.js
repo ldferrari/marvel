e9c3b72b70ed1771a0887eff63ab07137332c82d
ferrari <luisdiasferrari@gmail.com>
2021-04-07 22:10:27 -0300

    HTML com label Email e senha, botao para entrar e criar conta

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>teste</h1>
+      <LoginPage />
     </div>
   );
 }
--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,40 +1,6 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import GeneralContext from '../../context/general/GeneralContext';
-import loginData from '../../services/general/fetchLoginData';
-import validateEmail from '../../services/general/validateEmail';
-import validatePassword from '../../services/general/validatePassword';
-import '../../css/loginPage.css';
+import React from 'react';
 
-export default function LoginPage(props) {
-  const [emailValidate, setEmailValidate] = useState(false);
-  const [passwordValidate, setPasswordValidate] = useState(false);
-  const { userData, setUserData } = useContext(GeneralContext);
-
-  const login = async (data) => {
-    const usuario = await loginData(data);
-    setUserData({
-      ...userData,
-      id: usuario.id,
-      role: usuario.role,
-      name: usuario.name,
-      email: usuario.email,
-    });
-    localStorage.setItem('token', usuario.token);
-    localStorage.setItem(
-      'user',
-      JSON.stringify({
-        id: usuario.id,
-        email: usuario.email,
-        role: usuario.role,
-        name: usuario.name,
-      }),
-    );
-    if (usuario.role === 'administrator') return props.history.push('/admin/orders');
-    if (usuario.role === 'client') return props.history.push('/products');
-    return true;
-  };
+export default function LoginPage() {
 
   return (
     <div className="login">
@@ -46,12 +12,6 @@ export default function LoginPage(props) {
             id="email"
             data-testid="email-input"
             className="input"
-            onChange={ (event) => {
-              if (validateEmail(event.target.value)) {
-                setEmailValidate(true);
-                setUserData({ ...userData, email: event.target.value });
-              }
-            } }
           />
         </label>
         <label htmlFor="senha" className="label">
@@ -61,23 +21,13 @@ export default function LoginPage(props) {
             id="senha"
             data-testid="password-input"
             className="input"
-            onChange={ (event) => {
-              if (validatePassword(event.target.value)) {
-                setPasswordValidate(true);
-                setUserData({ ...userData, password: event.target.value });
-              }
-            } }
           />
         </label>
       </div>
       <button
         type="button"
         data-testid="signin-btn"
-        disabled={ !emailValidate || !passwordValidate }
         className="entrar"
-        onClick={ () => {
-          login(userData);
-        } }
       >
         ENTRAR
       </button>
@@ -86,12 +36,8 @@ export default function LoginPage(props) {
         data-testid="no-account-btn"
         className="semConta"
       >
-        <Link to="/register" className="link">Ainda não tenho conta</Link>
+        Ainda não tenho conta
       </button>
     </div>
   );
 }
-
-LoginPage.propTypes = {
-  history: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
