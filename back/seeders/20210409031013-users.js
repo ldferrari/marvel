0f57e90a8a3925d788fd136f63d9eaa48c5da140
ferrari <luisdiasferrari@gmail.com>
2021-04-17 18:35:10 -0300

    back

--- back/seeders/20210409031013-users.js
+++ back/seeders/20210409031013-users.js
@@ -11,7 +11,7 @@ module.exports = {
     ], {});
   },
 
-  down: async (queryInterface) => {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkDelete('Users', null, {});
   }
 };
